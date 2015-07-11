6d700e2c1f7b17b7fb10b7d055fc6d9376d07c12
Maaartin Grajcar <grajcar@seznam.cz>
2015-07-12 00:56:15 +0200

    .... nn

---
+++ src/maaartin/game/ai/neural/Neuralwork.java
@@ -0,0 +1,106 @@
+package maaartin.game.ai.neural;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Random;
+
+
+public final class Neuralwork {
+	private static final class Neuron {
+		Neuron(int axon, Random random, int arity) {
+			checkArgument(arity >= 2);
+			checkArgument(axon >= arity);
+			checkNotNull(random);
+			this.axon = axon;
+			weights = new float[arity];
+			for (int i=0; i<arity; ++i) weights[i] = random.nextFloat() - 0.5f;
+			synapses = new int[arity];
+			for (int i=0; i<arity; ) {
+				final int n = random.nextInt(axon);
+				if (synapsesBitSet.get(n)) continue;
+				synapsesBitSet.set(n);
+				synapses[i++] = n;
+			}
+		}
+
+		void eval(float[] values) {
+			values[axon] = sigmoid(linear(values));
+		}
+
+		void learn(float[] values, float[] deltas) {
+			final float d = sigmoidDerived(linear(values)) * deltas[axon];
+			for (int i=0; i<weights.length; ++i) {
+				final int j = synapses[i];
+				deltas[j] += d * weights[i];
+				weights[i] += d * values[j];
+			}
+		}
+
+		private float linear(float[] values) {
+			float result = 0;
+			for (int i=0; i<weights.length; ++i) result += weights[i] * values[synapses[i]];
+			return result;
+		}
+
+		private float sigmoid(float x) {
+			final float q = 0.5f + Math.abs(x);
+			return x / q;
+		}
+
+		private float sigmoidDerived(float x) {
+			final float q = 0.5f + Math.abs(x);
+			return 0.5f / (q*q);
+		}
+
+		private final int axon;
+		private final float[] weights;
+		private final int[] synapses;
+		private final BitSet synapsesBitSet = new BitSet();
+	}
+
+	public Neuralwork(int inLength, int hiddenLength, int outLength, Random random, int arity) {
+		checkArgument(inLength > 0);
+		checkArgument(hiddenLength >= 0);
+		checkArgument(outLength > 0);
+		checkNotNull(random);
+		checkArgument(arity>=3);
+		this.inLength = inLength;
+		this.hiddenLength = hiddenLength;
+		this.outLength = outLength;
+		neurons = new Neuron[hiddenLength + outLength];
+		for (int i=0, j=inLength+1; i<neurons.length; ++i, ++j) neurons[i] = new Neuron(j, random, Math.min(j, arity));
+		final int length = inLength + 1 + hiddenLength + outLength;
+		values = new float[length];
+		values[inLength] = 1;
+		deltas = new float[length];
+	}
+
+	public void input(float[] input) {
+		checkArgument(input.length == inLength);
+		System.arraycopy(input, 0, values, 0, inLength);
+		for (int i=0; i<neurons.length; ++i) neurons[i].eval(values);
+	}
+
+	public void output(float[] output) {
+		checkArgument(output.length == outLength);
+		System.arraycopy(values, inLength + 1 + hiddenLength, output, 0, outLength);
+	}
+
+	public void learn(float[] desired, float rate) {
+		checkArgument(desired.length == outLength);
+		Arrays.fill(deltas, 0);
+		for (int i=deltas.length-1, j=outLength-1; j>=0; --i, --j) deltas[i] = rate * (desired[j] - values[i]);
+		for (int i=neurons.length-1; i>=0; --i) neurons[i].learn(values, deltas);
+	}
+
+	private final int inLength;
+	private final int hiddenLength;
+	private final int outLength;
+
+	private final Neuron[] neurons;
+	private final float[] values;
+	private final float[] deltas;
+}
---
+++ src/maaartin/game/ai/neural/_NeuralworkTest.java
@@ -0,0 +1,91 @@
+package maaartin.game.ai.neural;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+import junit.framework.TestCase;
+
+public class _NeuralworkTest extends TestCase {
+	public void testAnd() {
+		check(10000, AND_PATTERNS, 1);
+	}
+
+	public void testXor() {
+		check(10000, XOR_PATTERNS, 4);
+	}
+
+	public void testBinary() {
+		check(10000, BINARY_PATTERNS, 5);
+	}
+
+	public void testTernary() {
+		check(10000, TERNARY_PATTERNS, 7);
+	}
+
+	private void check(int limit, float[][][] patterns, int hiddenLength) {
+		check(limit, patterns, hiddenLength, Integer.MAX_VALUE);
+	}
+
+	private void check(int limit, float[][][] patterns, int hiddenLength, int arity) {
+		patterns = patterns.clone();
+		final Random random = new Random(0);
+		for (int m=0; m<1000; ++m) {
+			Collections.shuffle(Arrays.asList(patterns), random);
+			final int inLength = patterns[0][0].length;
+			final int outLength = patterns[0][1].length;
+			final Neuralwork neuralwork = new Neuralwork(inLength, hiddenLength, outLength, random, arity);
+			final float[] output = new float[outLength];
+			for (int n=0; ; ++n) {
+				boolean success = true;
+				for (final float[][] io : patterns) {
+					final float[] input = io[0];
+					final float[] desired = io[1];
+					neuralwork.input(input);
+					neuralwork.output(output);
+					boolean valid = true;
+					for (int i=0; i<output.length; ++i) valid &= Math.abs(desired[i] - output[i]) < 0.25;
+					if (valid) continue;
+					success = false;
+					neuralwork.learn(desired, 0.2f);
+				}
+				if (success) break;
+				assertTrue("too many iterations", n<limit);
+			}
+		}
+	}
+
+	private static final float[][][] AND_PATTERNS = {
+		{{0, 0}, {0}},
+		{{0, 1}, {0}},
+		{{1, 0}, {0}},
+		{{1, 1}, {1}},
+	};
+
+	private static final float[][][] XOR_PATTERNS = {
+		{{0, 0}, {0}},
+		{{0, 1}, {1}},
+		{{1, 0}, {1}},
+		{{1, 1}, {0}},
+	};
+
+	// implication and nxor
+	private static final float[][][] BINARY_PATTERNS = {
+		{{0, 0}, {1, 1}},
+		{{0, 1}, {1, 0}},
+		{{1, 0}, {0, 0}},
+		{{1, 1}, {1, 1}},
+	};
+
+	// majority and xor
+	private static final float[][][] TERNARY_PATTERNS = {
+		{{0, 0, 0}, {0, 0}},
+		{{0, 0, 1}, {0, 1}},
+		{{0, 1, 0}, {0, 1}},
+		{{0, 1, 1}, {1, 0}},
+		{{1, 0, 0}, {0, 1}},
+		{{1, 0, 1}, {1, 0}},
+		{{1, 1, 0}, {1, 0}},
+		{{1, 1, 1}, {1, 1}},
+	};
+}
