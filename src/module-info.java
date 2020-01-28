dfa0c7326ace9e820af120b563ba5455a0a7e29f
Maaartin Grajcar <maaartinus@gmail.com>
2020-01-28 19:20:50 +0100

    Switch to module path.

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module ultimatoe {
+	exports maaartin.game;
+	exports maaartin.game.ultimatoe;
+	exports maaartin.game.ai;
+
+	requires java.desktop;
+	requires junit;
+	requires lombok;
+	requires com.google.common;
+	requires jsr305;
+	requires org.mapstruct.processor;
+}
